530cc6047acb9be1a69c94eae05e0b659fcf80d5
EbnulMahmood <shovonmahmood67@gmail.com>
2022-10-27 22:54:25 +0600

    add category entity

--- src/Inventory.Management.CoreBusiness/Entities/BaseEntity/BaseEntity.cs
+++ src/Inventory.Management.CoreBusiness/Entities/BaseEntity/BaseEntity.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Inventory.Management.CoreBusiness.Enums;
 
 namespace Inventory.Management.CoreBusiness.Entities.BaseEntity
 {
---
+++ src/Inventory.Management.CoreBusiness/Entities/Category.cs
@@ -0,0 +1,11 @@
+using Inventory.Management.CoreBusiness.Entities.BaseEntity;
+using Inventory.Management.CoreBusiness.Entities.BaseEntity.Contracts;
+
+namespace Inventory.Management.CoreBusiness.Entities
+{
+    public class Category : BaseEntity<Guid>, IEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
