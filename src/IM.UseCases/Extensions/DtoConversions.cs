61ea16412d061b593096a922bb1bbfbebf714b6a
EbnulMahmood <shovonmahmood67@gmail.com>
2022-11-04 16:17:57 +0600

    update-datatable-style

--- src/IM.UseCases/Extensions/DtoConversions.cs
+++ src/IM.UseCases/Extensions/DtoConversions.cs
@@ -41,7 +41,7 @@ namespace IM.UseCases.Extensions
         {
             string ConditionClassStatus(StatusDto statusDto)
             {
-                return statusDto == StatusDto.Active ? "text-success" : "text-danger";
+                return statusDto == StatusDto.Active ? "success" : "warning";
             }
 
             string ConditionTextStatus(StatusDto statusDto)
@@ -51,21 +51,21 @@ namespace IM.UseCases.Extensions
 
             string ActionLinks(Guid id)
             {
-                return $"<div class='w-25 btn-group' role='group'>" +
-                    $"<a href='Edit/{id}' class='btn btn-primary mx-2'><i class='bi bi-pencil-square'></i>Edit</a>" +
-                    $"<button type='button' data-bs-target='#deleteCategory' data-bs-toggle='ajax-modal'" +
-                        $"class='btn btn-danger mx-2 btn-category-delete' data-category-id='{id}'>" +
-                        $"<i class='bi bi-trash-fill'></i>Delete</button>" +
-                    $"<a href='Details/{id}' class='btn btn-secondary mx-2'><i class='bi bi-ticket-detailed-fill'>" +
-                        $"</i>Details</a>" +
-                    $"</div>";
+                return $"<div class='btn-group' role='group'>" +
+                            $"<a href='Edit/{id}' class='btn btn-primary mx-1'><i class='bi bi-pencil-square'></i>Edit</a>" +
+                            $"<button type='button' data-bs-target='#deleteCategory' data-bs-toggle='ajax-modal'" +
+                                $"class='btn btn-danger mx-1 btn-category-delete' data-category-id='{id}'>" +
+                                $"<i class='bi bi-trash-fill'></i>Delete</button>" +
+                            $"<a href='Details/{id}' class='btn btn-info mx-1'><i class='bi bi-ticket-detailed-fill'>" +
+                                $"</i>Details</a>" +
+                        $"</div>";
             }
 
             return (from category in categories
                     select new CategoryDto
             {
                 Name = category.Name,
-                StatusHtml = $"<span class='{ConditionClassStatus((StatusDto)category.Status)}'>" +
+                StatusHtml = $"<span class='badge alert-{ConditionClassStatus((StatusDto)category.Status)}'>" +
                              $"{ConditionTextStatus((StatusDto)category.Status)}</span>",
                 ActionLinkHtml = ActionLinks(category.Id),
             }).ToList();
